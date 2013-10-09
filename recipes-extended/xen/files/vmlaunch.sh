b0db19e61a13671fc5f322c1b2f6bfc8f1cae1f8
Peter Muller <stickmanthe@gmail.com>
2013-10-09 10:09:36 -0400

    Update to launch all VMs. Use actual scripting this time

--- recipes-extended/xen/files/vmlaunch.sh
+++ recipes-extended/xen/files/vmlaunch.sh
@@ -2,8 +2,8 @@
 #script to start existing VMs if desired
 #use only after starting the bridge with runme.sh
 
-cd /storage
-xl create ubuntu.cfg
-xl create centos.cfg
-xl create debian.cfg
-xl create windows.cfg
+
+for i in $(ls /storage/*.cfg)
+do
+    xl create $i
+done
